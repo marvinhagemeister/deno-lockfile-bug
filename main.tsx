e2d35e00921c6a50d3a60e0f2f6893a79a314646
Marvin Hagemeister <marvin@deno.com>
2024-08-29 14:20:36 +0200

    simplify repro

--- main.tsx
+++ main.tsx
@@ -1,15 +1,3 @@
 import { renderToString } from "preact-render-to-string";
 
-const classNameBuilder = new Proxy(() => {}, {
-  get(first, second) {
-    console.log(first, second);
-    return (f1, f2) => {
-      console.log(f1, f2);
-      return "example-class";
-    };
-  },
-});
-const element = <div className={classNameBuilder} />;
-// const element = <div className="foo" />;
-// <div></div>
-console.log(renderToString(element));
+console.log(renderToString(<div class="foo" />));
